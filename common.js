a570b84fa54c16c9c897582c1ce6bd748a3a6e98
prakhar_sahay <prakhar@brandeis.edu>
2014-08-24 19:55:29 -0400

    Relocated to GitHub
    Works in IE, FF, Chrome
    Except reload onresize not running in FF

---
+++ common.js
@@ -0,0 +1,68 @@
+function decrypt(){
+	var course;
+	for(var loop3=0;course=course_data[loop3];loop3++){// for each course_data string
+	    course=createClass(course.split(";"));
+	    // customize newdiv and then plot
+	    newdiv.style.backgroundColor=course.color;
+	    if(course.place!="???"){
+		    newdiv.title=Course.place;
+		}
+	    if(course.trans){
+	        newdiv.style.opacity=0.5;
+	    }
+
+	    for(var loop4=0;loop4<=course.times.length-1;loop4++){// for each time span, make block
+	        plot(newdiv,course.times[loop4],course.name);
+	    }
+	}
+}
+
+// given styled div and "1~13~14" produce its block
+function plot(div_template,time_str,name){
+	var div=div_template.cloneNode();
+	div.textContent=name;
+
+	var time_arr=time_str.split("~");// ["1","13","14"]
+	var hours=parseFloat(time_arr[2])-parseFloat(time_arr[1]);// 14-13
+
+	div.style.width=days_width; 
+	div.style.height=(hours*(time_height+2)-2)+"px";
+	div.style.lineHeight=div.style.height;
+	div.style.top=2+(time_height+2)*(parseFloat(time_arr[1])-8)+"px";
+	div.style.left=(days_width+20)*(parseInt(time_arr[0],10)+1)+"px";// +20 for padding
+	contain.appendChild(div);
+}
+
+function Class(name,color,time_str,place){// four strings from split(';')
+	this.name=name.substring(this.trans=(name[0]=='z'));// set boolean .trans,set name 
+    this.color=color;
+    this.place=place;
+    this.times=[];
+    var times=time_str.split(',');
+
+    for(var i=0;i<times.length;i++){// for each in "134(13~14),5(12.5~14)"
+    	var index=times[i].indexOf("(");
+    	if(index>-1){// multiday push
+    		pushAllDays(this.times,times[i],index);
+    	}else{
+    		this.times.push(times[i]);// normal push
+    	}
+    }
+}
+
+function createClass(data_arr){
+	return new Class(data_arr[0],data_arr[1],data_arr[2],data_arr[3]);
+}
+
+
+function pushAllDays(array,multiday_str,index){
+    var day_numbers=multiday_str.substring(0,index);// "134"
+    var hours=multiday_str.substring(index+1,multiday_str.length-1);// "13~14"
+    for(var i=0;i<day_numbers.length;i++){
+    	array.push(day_numbers[i]+"~"+hours);
+    }
+}
+
+function newDiv(){
+	return document.createElement("div");
+}
---
+++ dom.js
@@ -0,0 +1,55 @@
+var days_width,days_height,time_height;
+
+var newdiv=newDiv();
+newdiv.className="block";
+
+function setup(){
+	// adjust grey container, 25px padding
+	var containDays=document.getElementById("containDays");
+	var contain=document.getElementById("contain");
+	var contain_width=Math.floor((window.innerWidth-66)/8)*8;// -50 for space, -16 for body, fixed as multiple of 8
+	var contain_height=Math.floor((window.innerHeight-108)/16)*16+2;// -50 for space, -16 for body, -42 for header (16 times)
+	containDays.style.width=contain_width;
+	contain.style.width=contain_width;
+	contain.style.height=contain_height;
+
+	// adjust days headers
+	var days=containDays.getElementsByClassName("days");
+	days_width=contain_width/8-20;
+	days_height=20;
+
+	for(var i=0;i<days.length;i++){
+		days[i].style.width=days_width;
+		days[i].style.height=days_height;
+	}
+
+	// adjust line left and width
+	var line=newDiv();
+	line.className="line";
+	line.style.width=7/8*contain_width;
+	line.style.left=contain_width/8;
+
+	var time=newDiv();
+	time.className="time";
+	time_height=(contain_height-2)/16-2;
+	time.style.height=time.style.lineHeight=time_height+"px";
+
+	// add pairs of lineNode>line and timeNode>time
+
+	var lineNode=newDiv();
+	lineNode.appendChild(line.cloneNode());
+	var timeNode=newDiv();
+	timeNode.style.width=contain_width/8;
+	timeNode.appendChild(time.cloneNode());
+
+	for(var j=0;j<16;j++){
+		contain.appendChild(lineNode.cloneNode(true));
+		timeNode.firstChild.textContent=(j+8)%12+"-"+(j+9)%12;
+		contain.appendChild(timeNode.cloneNode(true));
+	}
+	contain.appendChild(lineNode.cloneNode(true));
+}setup();
+
+window.onresize=function(){location.reload();console.log(5)};
+// decrypt-->createClass,plot
+decrypt();
