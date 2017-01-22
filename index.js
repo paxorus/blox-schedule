9fdac8f4a35ea3f67fc1435812af0c5b6c9bb349
paxorus <prakhar@brandeis.edu>
2017-01-22 00:22:25 -0500

    Fix Heroku compatibility
    Fix "Internal Server Error" on Heroku dyno.

--- index.js
+++ index.js
@@ -32,63 +32,6 @@ app.get('/:scheduleId', function(req, res) {
 	res.render('pages/', {scheduleId: scheduleId});
 });
 
-// app.get('/branch/:parentId', function(req, res) {
-// 	var parentId = req.params.parentId;
-// 	res.render('pages/branch', {parentId: parentId});
-// });
-
-// app.get('/branch', function(req, res) {
-// 	res.render('pages/branch', {parentId: 'root'});
-// });
-
-// app.get('/leaf/:issueId', function(req, res) {
-// 	var issueId = req.params.issueId;
-// 	res.render('pages/leaf', {issueId: issueId});
-// });
-
-// app.get('/leaf', function(req, res) {
-// 	res.render('pages/leaf', {issueId: 'root'});
-// });
-
-// app.get('/db', function (req, res) {
-// 	Mongo.find('new', function (docs) {
-// 		res.render('pages/db', {docs: docs});
-// 	});
-// });
-
-// app.put('/db', function (req, res) {
-// 	var dbInsert = function (data) {
-// 		Mongo.insert('new', data, function (success) {
-// 			res.send(success);
-// 		});
-// 	};
-
-// 	// read in PUT body
-// 	read(req, dbInsert);
-// });
-
-// app.delete('/db', function (req, res) {
-// 	var dbDelete = function (data) {
-// 		Mongo.delete('new', data, function (success) {
-// 			res.send(success)
-// 		});
-// 	}
-
-// 	// read in DELETE body
-// 	read(req, dbDelete);
-// });
-
-// function read(req, terminate) {
-// 	var body = [];
-// 	req.on("data", function (chunk) {
-// 		body.push(chunk);
-// 	}).on("end", function () {
-// 		body = Buffer.concat(body).toString();
-// 		var data = querystring.parse(body);
-// 		terminate(data);
-// 	});
-// }
-
 // for the server console
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
